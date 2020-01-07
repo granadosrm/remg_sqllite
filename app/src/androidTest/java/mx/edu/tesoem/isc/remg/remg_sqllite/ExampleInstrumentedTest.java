1aae7e2d0e73e204cf616bd4a0aae42b7d7538bf
ISC Tesoem <tesoem@iMac-Tesoem-105.local>
2020-01-07 13:20:09 -0600

    Initial commit

---
+++ app/src/androidTest/java/mx/edu/tesoem/isc/remg/remg_sqllite/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package mx.edu.tesoem.isc.remg.remg_sqllite;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("mx.edu.tesoem.isc.remg.remg_sqllite", appContext.getPackageName());
+    }
+}
